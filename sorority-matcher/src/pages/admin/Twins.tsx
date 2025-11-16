38729b2567c4955c83e8ce14170cb3182377ea12
Yufei Huang <yh5154@nyu.edu>
2025-11-15 22:55:36 -0500

    added title link for twins page

--- sorority-matcher/src/pages/admin/Twins.tsx
+++ sorority-matcher/src/pages/admin/Twins.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useMatching } from '../../contexts/MatchingContext';
+import { Link } from 'react-router-dom';
 
 const Twins = () => {
   const navigate = useNavigate();
@@ -8,7 +9,9 @@ const Twins = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-8">
       <header className="mb-12">
+        <Link to="/">
         <h1 className="text-4xl font-bold text-center">Sorora</h1>
+        </Link>
       </header>
 
       <div className="max-w-2xl w-full bg-white rounded-lg shadow-lg p-8">
